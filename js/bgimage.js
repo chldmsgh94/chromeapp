3e7647dc8716317bc8373c54dd89d06c6a7e09a4
Eunho <67308435+chldmsgh94@users.noreply.github.com>
2022-09-12 02:04:08 +0900

    Update bgimage.js

--- js/bgimage.js
+++ js/bgimage.js
@@ -2,4 +2,4 @@ const body = document.querySelector("body");
 const img = ["bg_0.jpeg", "bg_1.jpeg", "bg_2.jpeg"];
 const randumNum = Math.floor(Math.random() * img.length);
 
-body.style.backgroundImage = `url(/image/${img[randumNum]})`;
+body.style.backgroundImage = `url(image/${img[randumNum]})`;
